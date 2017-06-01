ff19c91c03ff56ec8cc3f6eb1a3a394099ae5e23
weydenherrera <weyden.herrera@tooltwist.com>
2017-06-01 09:42:29 +0800

    hello weydz to hello world

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var app = express();
 var port = process.env.port || 1337
 
 app.get('/', function (req, res) {
-    res.send('Hello Weydz!');
+    res.send('Hello world!');
 });
 
 var server = app.listen(port, function () {
