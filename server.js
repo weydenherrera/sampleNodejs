603e6b50dce9981b1476412df65271436fe75fbf
weydenherrera <weyden.herrera@tooltwist.com>
2017-06-29 14:57:19 +0800

    port changed to PORT

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 ﻿var express = require('express');
 var app = express();
-var port = process.env.port || 1337
+var port = process.env.PORT || 1337
 
 app.get('/', function (req, res) {
     res.send('Hello world!');
